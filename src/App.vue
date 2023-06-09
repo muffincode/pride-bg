5bcf50d7a2b7d1d688f0907587dcb2a31f5076ce
Camille Fite <camille.fite@gmail.com>
2023-06-09 13:53:07 +0200

    Using canvas element, generating wavy images

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import axios from 'axios'
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView, createWebHashHistory } from 'vue-router'
 import { onMounted, ref } from 'vue';
 
 const searchQuery = ref('lesbian_2019')
@@ -33,11 +33,14 @@ async function fetch() {
     .then((res) => {
       flagsData.value = res.data
       updateBackground()
+      drawInCanvas()
     })
 }
 
 function updateBackground() {
   currFlagColours.value = flagsData.value.find(e => e.id == searchQuery.value).colors
+
+  drawInCanvas()
 
   if (backgroundStyle.value == "gradient") {
     cssGradient.value = generateLinearGradient(currFlagColours.value)
@@ -53,31 +56,81 @@ function updateBackground() {
       let percentage31 = 100 - percentage3;
       let percentage41 = 100 - percentage4;
       c.randomRadius = `${percentage1}% ${percentage11}% ${percentage21}% ${percentage2}% / ${percentage3}% ${percentage4}% ${percentage41}% ${percentage31}%`
-      c.posLeft = percentage1+"vh"
-      c.posTop = percentage3+"vh"
+      c.posLeft = percentage1 + "vh"
+      c.posTop = percentage3 + "vh"
     })
   }
 }
 
+function rand(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function drawInCanvas() {
+  const canvas = document.getElementById('canvas');
+
+  const h = 851
+  const w = 393
+  const margin = 100
+  const nbWaves = currFlagColours.value.length
+
+  if (canvas.getContext) {
+    canvas.width = w
+    canvas.height = h
+    const ctx = canvas.getContext('2d');
+
+    let increment = h / nbWaves
+    currFlagColours.value.forEach((c, index) => {
+      let pos = increment * (index + 1) + rand(-50, 0)
+      ctx.fillStyle = c.hexCode;
+
+      ctx.beginPath();
+      ctx.moveTo(0, pos);
+      ctx.bezierCurveTo(0 + (w / rand(2, 15)), pos - rand(20, 300), w - (w / rand(2, 15)), pos - rand(20, 300), w, pos)
+      ctx.fill();
+      ctx.fillRect(0, pos - 1, w, h - (h / pos) + 1);
+      if (index === 0) {
+        ctx.fillRect(0, 0, w, h);
+      }
+    })
+
+  }
+  /*
+  let image = canvas.toDataURL("image/png");
+  document.write('<img src="'+image+'"/>');
+  */
+}
+
 onMounted(() => {
   fetch()
+
 })
 </script>
 
 <template>
   <section>
-    <div class="shapes-container" v-if="backgroundStyle == 'blobs'">
+
+    <canvas id="canvas">
+
+    </canvas>
+
+    <!-- <div class="shapes-container" v-if="backgroundStyle == 'blobs'">
       <div class="shape" v-for="color in currFlagColours" :id="color.id"
-        :style="{ background: color.hexCode, borderRadius: color.randomRadius, left: color.posLeft, top: color.posTop }"></div>
-    </div>
+        :style="{ background: color.hexCode, borderRadius: color.randomRadius, left: color.posLeft, top: color.posTop }">
+      </div>
+    </div> -->
     <main>
-      <select v-model="searchQuery" id="" @change="updateBackground">
+      <select v-model="searchQuery" id="" @change="updateBackground()">
         <option v-for="flag in flagsData" :value="flag.id">{{ flag.name }} ({{ flag.year }})</option>
       </select>
 
-      <ul>
-        <li v-for="c in currFlagColours" :style="{ background: c.hexCode }">{{ c.name }}</li>
-      </ul>
+      <div class="colours">
+        <div v-for="c in currFlagColours" :style="{ background: c.hexCode }">{{ c.name }}</div>
+      </div>
+
+      <br>
+
+      <button @click="updateBackground()">Generate</button>
     </main>
 
   </section>
@@ -86,18 +139,25 @@ onMounted(() => {
 </template>
 
 <style scoped lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Geologica:wght@100;400;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap');
 
 html,
 body,
 #app {
-  font-family: Geologica, Arial, serif;
+  font-family: "Playfair Display", Arial, serif;
   margin: 0;
   padding: 0;
 }
 
 * {
-  font-family: Geologica;
+  font-family: "Playfair Display";
+}
+
+canvas {
+  background: grey;
+  border: 5px solid white;
+  border-radius: 10px;
+  box-shadow: 0 0 50px rgba(0, 0, 0, 0.2);
 }
 
 section {
@@ -109,6 +169,7 @@ section {
   display: flex;
   align-items: center;
   justify-content: center;
+  gap: 2em;
 
   .shape {
     width: 33vw;
@@ -129,6 +190,23 @@ section {
 
     select {
       padding: 0.5em;
+      width: 100%;
+      font-weight: bold;
+      font-size: 100%;
+    }
+
+    div.colours {
+      margin-top: 1em;
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      gap: 0.3em;
+      div {
+        padding: 1em;
+        text-align: center;
+        border-radius: 3px;
+        color: #eaeaea;
+        box-shadow: 0 0 2px rgba(0,0,0,0.1);
+      }
     }
   }
 }</style>
